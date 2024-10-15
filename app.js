6dd3209e2e8f23f223a76377448da67236f5614e
Muhammed Dulger <dlgr79@yahoo.com>
2024-10-16 00:14:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Merhaba, Node.js dünyası!\n');
+  res.end('Merhaba, Yeni Code!\n');
 });
 
 // Sunucuyu başlat
