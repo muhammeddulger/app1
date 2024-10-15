0e37cad9ceca8b6ee1ad61cd52e6da07db02b84e
Ubuntu <ubuntu@ip-172-31-41-178.ec2.internal>
2024-10-15 22:02:33 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// Sunucuyu oluştur
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Merhaba, Node.js dünyası!\n');
+});
+
+// Sunucuyu başlat
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
